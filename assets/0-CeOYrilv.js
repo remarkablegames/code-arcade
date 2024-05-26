7fef4e1b1432253be0641d34faedfdd5538c9ddc
remarkablemark <remarkablemark@users.noreply.github.com>
2024-05-26 04:39:12 +0000

    Deploying to gh-pages from @ remarkablegames/code-arcade@0be0c24b9a843bb5e28b887252ff7b839fb64a02 🚀

---
+++ assets/0-CeOYrilv.js
@@ -0,0 +1,7 @@
+function r(){loadSprite("player","sprites/bean.png")}const e=`
+add([
+  sprite('player'),
+  pos(80, 40),
+  area(),
+])
+`;function p(){}export{p as afterScript,r as beforeScript,e as script};
