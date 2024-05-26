71c1451d7d5eca09c4c762ae027caab9da976c67
remarkablemark <remarkablemark@users.noreply.github.com>
2024-05-26 23:31:26 +0000

    Deploying to gh-pages from @ remarkablegames/code-arcade@6b2694326fdf4582c8e46943eb37cfec78fdaa99 🚀

---
+++ assets/1-7BaEw7N8.js
@@ -0,0 +1,7 @@
+import{S as e,a as t}from"./sprite-DO8_xoiY.js";import{S as r}from"./index-CueixKFA.js";const i=1;const a="Comments";function p(){loadSprite(e.player,"sprites/bean.png");loadSprite(e.exit,"sprites/door.png");t(add([sprite(e.player),pos(50,50),area(),e.player]));onCollide(e.player,e.exit,()=>{go(r.game,0)});add([text("Where's the exit?")])}const n=`
+/**
+ * Single line comments start with two forward slashes //
+ */
+
+// add([sprite('exit'), pos(center()), area(), 'exit'])
+`;function l(){}export{i as level,l as postscript,p as prescript,n as script,a as title};
