8b32501b7de4efbc15820ea15593f655e642cf79
remarkablemark <remarkablemark@users.noreply.github.com>
2024-05-27 06:03:02 +0000

    Deploying to gh-pages from @ remarkablegames/code-arcade@aff1fecb654a85dfde4ceab538ee3ab7c323cca6 🚀

---
+++ assets/0-VC3d5Tub.js
@@ -0,0 +1,5 @@
+import{a as r}from"./cursors-Ms_gDOeS.js";import{i as e,S as t}from"./index-CK0hfUCJ.js";const o=0;const a="Hello, World!";function p(){e(o);r(add([sprite(t.player),pos(50,50),area(),t.player]));add([sprite(t.exit),pos(500,500),area(),t.exit])}const d=`
+add([
+  text('WASD or arrow keys to move')
+])
+`;function l(){}export{o as level,l as postscript,p as prescript,d as script,a as title};
