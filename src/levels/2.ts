3aff8a584239f53b0392b78d4d598c932db55db8
Mark <mark@remarkablemark.org>
2024-05-26 20:32:46 -0400

    feat(levels): add level 2

--- src/levels/2.ts
+++ src/levels/2.ts
@@ -1,30 +1,32 @@
 import { addCursorKeys } from '../helpers'
 import { Scene, Sprite } from '../types'
 
-export const level = 1
-export const title = 'Comments'
+export const level = 2
+export const title = 'Strings'
 
 export function prescript() {
   loadSprite(Sprite.player, 'sprites/bean.png')
   loadSprite(Sprite.exit, 'sprites/door.png')
 
   addCursorKeys(
-    add([sprite(Sprite.player), pos(50, 50), area(), Sprite.player]),
+    add([sprite(Sprite.player), pos(center()), area(), Sprite.player]),
   )
 
   onCollide(Sprite.player, Sprite.exit, () => {
-    go(Scene.game, level + 1)
+    go(Scene.game, 0)
   })
-
-  add([text("Where's the exit?")])
 }
 
 export const script = `
 /**
- * Single line comments start with two forward slashes //
+ * Strings are text inside single or double quotes
  */
 
-// add([sprite('exit'), pos(center()), area(), 'exit'])
+add([
+  sprite('exite'),
+  area(),
+  "exit",
+])
 `
 
 export function postscript() {}
