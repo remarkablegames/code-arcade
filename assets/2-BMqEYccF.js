0d97077a5a259d1ab5c604f8d251bd160e9c1c9f
remarkablemark <remarkablemark@users.noreply.github.com>
2024-05-27 06:26:43 +0000

    Deploying to gh-pages from @ remarkablegames/code-arcade@ccb3d276317e56269ac6a7cd69b92e341d5646b5 🚀

---
+++ assets/2-BMqEYccF.js
@@ -0,0 +1,11 @@
+import{a as t}from"./cursors-Ms_gDOeS.js";import{i as r,S as e}from"./index-BfM8vk18.js";const i=2;const a="Strings";function p(){r(i);t(add([sprite(e.player),pos(center()),area(),e.player]))}const n=`
+/**
+ * Strings are text inside single or double quotes
+ */
+
+add([
+  sprite('exite'),
+  area(),
+  "exit",
+])
+`;function c(){}export{i as level,c as postscript,p as prescript,n as script,a as title};
