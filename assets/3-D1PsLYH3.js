3fd6dcb31d5c55886740055e3d6ff6effb2a9719
remarkablemark <remarkablemark@users.noreply.github.com>
2024-05-28 07:03:49 +0000

    Deploying to gh-pages from @ remarkablegames/code-arcade@52b5ab5c9342b5ec73a63e4dccbd82a671eec77a 🚀

---
+++ assets/3-D1PsLYH3.js
@@ -0,0 +1,12 @@
+import{a as r}from"./cursors-BIrzN-a5.js";import{i as s,S as e}from"./index-DUn6C2sZ.js";const i=3;const a="Numbers";const t=[];function n(){s(i,t);t.push(r(add([sprite(e.player),pos(500,500),area(),e.player])).cancel);add([text("Exit is not in view?")])}const c=`
+/**
+ * Numbers represent floating-point numbers like 42 or -13.37.
+ */
+
+add([
+  sprite('exit'),
+  area(),
+  'exit',
+  pos(-9999, -9999),
+])
+`;function l(){}export{i as level,l as postscript,n as prescript,c as script,a as title};
