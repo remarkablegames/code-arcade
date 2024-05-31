deafcd9c13e17af86231d8473d40d220a3f1c2ff
Mark <mark@remarkablemark.org>
2024-05-30 21:24:02 -0400

    feat(levels): add 19

---
+++ src/levels/19.ts
@@ -0,0 +1,47 @@
+import {
+  addText,
+  loadExit,
+  loadKey,
+  loadPlayer,
+  registerPasswordCheck,
+  registerPlayerKeys,
+  registerWinCondition,
+} from '../templates'
+
+export const level = 19
+export const title = 'Fullfilled Promise'
+
+const password = btoa(String(Date.now()))
+
+export const prescript = `
+${loadPlayer()}
+${loadExit()}
+${loadKey()}
+
+add([sprite('player'), pos(100, 100), area(), 'player'])
+add([sprite('key'), pos(center()), area(), 'key', { promise: Promise.resolve('${password}') }])
+
+${registerPlayerKeys()}
+${registerWinCondition(level)}
+${registerPasswordCheck(password)}
+
+${addText('Pass the Promise')}
+`
+
+export const script = `
+/**
+ * A Promise produces a value in the future
+ * If a Promise succeeded, it will produce a resolved value
+ */
+
+const key = get('key')[0]
+
+// example of a successful Promise
+const examplePromise = Promise.resolve('some value')
+examplePromise.then((value) => {
+  console.log(value)
+})
+
+// set \`key.password\` to the resolved value of \`key.promise\`
+console.log('is promise?', key.promise instanceof Promise)
+`
