1e5817e25019f4768429450f9bc17cef0cf367b3
Mark <mark@remarkablemark.org>
2024-06-02 01:48:54 -0400

    feat(levels): add 23

---
+++ src/levels/23.ts
@@ -0,0 +1,43 @@
+import { password } from '../../public/data/password.json'
+import {
+  addKey,
+  addPlayer,
+  addText,
+  loadExit,
+  registerPasswordCheck,
+  registerPlayerMovement,
+  registerWinCondition,
+} from '../templates'
+
+export const level = 23
+export const title = 'fetch'
+export const hint = 'Google JavaScript Fetch API'
+
+export const prescript = `
+${loadExit()}
+
+${addPlayer({ pos: '100, 100' })}
+${addKey({
+  pos: 'center()',
+  obj: JSON.stringify({ password }),
+})}
+
+${registerPlayerMovement()}
+${registerWinCondition(level)}
+${registerPasswordCheck(password)}
+
+${addText('Fetch the password')}
+`
+
+export const script = `
+/**
+ * fetch() makes a request for a resource
+ */
+
+const key = get('key')[0]
+
+// GET the password from "/data/password.json"
+let password
+
+key.password = password
+`
